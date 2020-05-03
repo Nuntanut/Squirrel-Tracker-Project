7d0d7f32aa7d550235abff4f6ee1e4e4e7b19a0f
Nuntanut Raksasri <nuntanutraksasri@Nuntanuts-MacBook-Pro.local>
2020-05-03 16:58:45 -0400

    Update Tracker urls and views

--- mysite/Tracker/views.py
+++ mysite/Tracker/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Squirrel
+
 def index(request):
     return HttpResponse('Hello, This is tracker index')
+
+def list_pets(request):
+    squirrels = Squirrel.objects.all()
+    
+    return render(request, 'Tracker/list.html', {'squirrels': squirrels})
+
+def get_squirrel(request, squirrel_id):
+
+    squirrel = Squirrel.opjects.get(id=squirrel_id)
+
+    return HttpResponse(f'Hello, This is {squirrel.unique_squirrel_id}')
 # Create your views here.
