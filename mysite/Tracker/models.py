966702a94097c5919fcc827a660df0e69a675333
Nuntanut Raksasri <nuntanutraksasri@Nuntanuts-MacBook-Pro.local>
2020-05-03 16:00:50 -0400

    Add initial Squirrel model

--- mysite/Tracker/models.py
+++ mysite/Tracker/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+class Squirrel(models.Model):
+
+    def __str__(self):
+        return self.unique_squirrel_id
+    
+    unique_squirrel_id = models.CharField(
+	'ID',
+        max_length = 100,
+        #help_text = _('Unique Squirrel ID'),
+        unique = True,
+        primary_key = True,
+    )
+
+    date = models.DateTimeField(
+	'Date',
+        #help_text = _('Date'),
+        null = True,
+    )
 # Create your models here.
