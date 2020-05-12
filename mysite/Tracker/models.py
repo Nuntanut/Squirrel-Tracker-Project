611e1bb5cdedea63c45ad22e5067b8f8fc977dba
Nuntanut Raksasri <nuntanutraksasri@Nuntanuts-MacBook-Pro.local>
2020-05-12 02:55:27 -0400

    Update Tracker Squirrel model

--- mysite/Tracker/models.py
+++ mysite/Tracker/models.py
@@ -6,11 +6,131 @@ class Squirrel(models.Model):
         return self.unique_squirrel_id
     
     unique_squirrel_id = models.CharField(
-	'ID',
-        max_length = 100,
-        unique = True,
-        primary_key = True,
+            'ID',
+            max_length = 100,
+            unique = True,
+            primary_key = True,
+    )
+    
+    latitude = models.FloatField('latitude', max_length = 20)
+
+    longitude = models.FloatField('longitude', max_length = 20)
+
+    shift_choices = (
+        ('AM', 'AM'),
+        ('PM', 'PM'),
     )
 
-    date = models.DateTimeField('Date',)
-# Create your models here.
+    shift = models.CharField(
+            'shift',
+            max_length = 2,
+            choices = shift_choices,
+            default = 'AM',
+    )
+
+    date = models.DateTimeField('Date')
+
+    age = models.CharField('Age')
+
+    primary_fur_color = models.CharField('Primary Fur Color', max_length = 100)
+
+    specific_location = models.CharField('Specific Location', max_length = 100)
+
+    TF_choices = (
+        ('true', 'True'),
+        ('false', 'False'),
+        ('', ''),
+    )
+
+    running = models.CharField(
+            'running',
+            max_length = 10,
+            choices = TF_choices,
+            default = '',
+    )
+
+    chasing = models.CharField(
+            'chasing',
+            max_length = 10,
+            choices = TF_choices,
+            default = '',
+    )
+
+    climbing = models.CharField(
+            'climbing',
+            max_length = 10,
+            choices = TF_choices,
+            default = '',
+    )
+
+    eating = models.CharField(
+            'eating',
+            max_length = 10,
+            choices = TF_choices,
+            default = '',
+    )
+
+    foraging = models.CharField(
+            'foraging',
+            max_length = 10,
+            choices = TF_choices,
+            default = '',
+    )
+
+    other_activities = models.CharField('other activities', max_length = 100)
+
+    kuks = models.CharField(
+            'kuks',
+            max_length = 10,
+            choices = TF_choices,
+            default = '',
+    )
+
+    quaas = models.CharField(
+            'quaas',
+            max_length = 10,
+            choices = TF_choices,
+            default = '',
+    )
+
+    moans = models.CharField(
+            'moans',
+            max_length = 10,
+            choices = TF_choices,
+            default = '',
+    )
+
+    tail_flags = models.CharField(
+            'tail flags',
+            max_length = 10,
+            choices = TF_choices,
+            default = '',
+    )
+
+    tail_twitches = models.CharField(
+            'tail twitches',
+            max_length = 10,
+            choices = TF_choices,
+            default = '',
+    )
+
+    approaches = models.CharField(
+            'approaches',
+            max_length = 10,
+            choices = TF_choices,
+            default = '',
+    )
+
+    indifferent = models.CharField(
+            'indifferent',
+            max_length = 10,
+            choices = TF_choices,
+            default = '',
+    )
+
+    runs_from = models.CharField(
+            'runs_from',
+            max_length = 10,
+            choices = TF_choices,
+            default = '',
+    )
